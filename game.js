b340b8469a03e8915408c138b8e99a50f6a4d159
Shivani-1611 <100325338+Shivani-1611@users.noreply.github.com>
2022-05-16 15:04:15 +0530

    Update game.js

--- game.js
+++ game.js
@@ -10,16 +10,53 @@ function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
     switch(randomNumber){
         case 0:
+            console.log('computer choice: rock');
             return 'rock';
             
         case 1:
+            console.log('computer choice: paper');
             return 'paper';
             
         case 2:
+            console.log('computer choice: scissor');
             return 'scissor';
             
-            
+        default:
+            break;
     }
     
 }
-console.log(getComputerChoice());
+function getWinner(userChoice,computerChoice){
+    if(userChoice===computerChoice){
+        return 'Match tied';
+    }
+    else if(userChoice='rock'){
+        if(computerChoice==='paper'){
+            return 'Computer Won';
+        }
+        else{
+            return 'You won';
+        }
+    }
+    else if(userChoice='paper'){
+        if(computerChoice==='scissor'){
+            return 'Computer Won';
+        }
+        else{
+            return 'You won';
+        }
+    }
+    else if(userChoice='scissor'){
+        if(computerChoice==='rock'){
+            return 'Computer Won';
+        }
+        else{
+            return 'You won';
+        }
+    }
+}
+
+console.log(getWinner(getUserChoice('rock'), getComputerChoice()));
+
+
+
